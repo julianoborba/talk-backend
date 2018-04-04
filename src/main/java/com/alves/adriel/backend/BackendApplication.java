e496a259938df6d80594886fe9cf6fb48f5341ba
Kleber Almeida Toyota <klebertiko@gmail.com>
2018-04-04 17:53:58 -0300

    sadaf

--- src/main/java/com/alves/adriel/backend/BackendApplication.java
+++ src/main/java/com/alves/adriel/backend/BackendApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class BackendApplication {
-
     public static void main(String[] args) {
-
         SpringApplication.run(BackendApplication.class, args);
     }
 }
